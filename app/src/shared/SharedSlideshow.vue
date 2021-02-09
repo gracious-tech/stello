478a0b24bb45e74eac2bfac0218cf1bbe8c02b79
shadow-light <noreply@gracious.tech>
2021-02-09 21:37:39 +1100

    Improve image placeholder

--- app/src/shared/SharedSlideshow.vue
+++ app/src/shared/SharedSlideshow.vue
@@ -51,9 +51,9 @@ export default {
     created(){
         // Generate a placeholder image
         const svg = `
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-            <path d="M0 0h24v24H0z" fill="#77777722"/>
-            <path fill="#00000022" d="M4 4h7V2H4c-1.1 0-2 .9-2 2v7h2V4zm6 9l-4 5h12l-3-4-2.03 2.71L10 13zm7-4.5c0-.83-.67-1.5-1.5-1.5S14 7.67 14 8.5s.67 1.5 1.5 1.5S17 9.33 17 8.5zM20 2h-7v2h7v7h2V4c0-1.1-.9-2-2-2zm0 18h-7v2h7c1.1 0 2-.9 2-2v-7h-2v7zM4 13H2v7c0 1.1.9 2 2 2h7v-2H4v-7z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="24" viewBox="0 0 48 24">
+            <path d="M0 0h48v24H0z" fill="#55555522"/>
+            <path fill="#55555533" d="m14 4c-1.1 0-2.2157 0.92136-2 2v5h1v-5c0-0.4714 0.5286-1 1-1h9v-1zm11 0v1h9c0.4714 0 1.0924 0.53775 1 1v5h1v-5c0-1.1-0.9-2-2-2zm2.5 4c-0.83 0-1.5 0.67-1.5 1.5s0.67 1.5 1.5 1.5 1.5-0.67 1.5-1.5-0.67-1.5-1.5-1.5zm-6.5 2-5 6h16l-4-4-3.0293 2.7109zm-9 3v5c0 1.1 0.9 2 2 2h9v-1h-9c-0.4714 0-1.0924-0.53775-1-1v-5zm23 0v5c0 0.4714-0.5286 1-1 1h-9v1h9c1.1 0 2-0.9 2-2v-5z"/>
         </svg>`
         const blob = new Blob([svg], {type: 'image/svg+xml'})
         this.placeholder = URL.createObjectURL(blob)
