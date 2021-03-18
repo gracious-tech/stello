73c58d2a5a840cae8e20d700837ce4ac47886468
shadow-light <noreply@gracious.tech>
2021-03-18 21:37:05 +1100

    Improve RouteContactsItem

--- app/src/components/routes/assets/RouteContactsItem.vue
+++ app/src/components/routes/assets/RouteContactsItem.vue
@@ -3,31 +3,49 @@
 
 v-list-item(:to='to')
     v-list-item-action
-        app-btn-checkbox(@click.prevent='toggle_selected' :value='contact.selected')
+        app-btn-checkbox(@click.prevent='toggle_selected' :value='item.selected')
     v-list-item-content
-        v-list-item-title {{ contact.display }}
+        v-list-item-title {{ item.contact.display }}
+    v-list-item-action.address(class='ellipsis')
+        v-list-item-subtitle {{ item.contact.address }}
+    v-list-item-action(v-if='issuer')
+        app-svg(:name='`icon_${issuer}`')
 
 </template>
 
 
 <script lang='ts'>
 
+import {RawLocation} from 'vue-router'
 import {Component, Vue, Prop} from 'vue-property-decorator'
+
+import {Contact} from '@/services/database/contacts'
+import {partition} from '@/services/utils/strings'
 
 
 @Component({})
 export default class extends Vue {
 
-    @Prop() contact
+    @Prop() item:{contact:Contact, selected:boolean}
 
-    get to(){
-        return {name: 'contact', params: {contact_id: this.contact.id}}
+    get to():RawLocation{
+        // Return route location for viewing the contact
+        return {
+            name: 'contact',
+            params: {contact_id: this.item.contact.id},
+        }
+    }
+
+    get issuer():string{
+        // Get issuer code if has a service account
+        const service_account = this.item.contact.service_account
+        return service_account && partition(service_account, ':')[0]
     }
 
     toggle_selected(){
-        this.contact.selected = !this.contact.selected
+        // Toggle the selected status of the contact item
+        this.item.selected = !this.item.selected
     }
-
 }
 
 </script>
@@ -36,10 +54,20 @@ export default class extends Vue {
 <style lang='sass' scoped>
 
 .v-list-item
+    // Show selection checkbox on hover of list item
     .v-btn
         visibility: hidden
     &:hover
         .v-btn
             visibility: visible
+
+    .v-list-item__action
+        // Make list more dense by removing excessive margins
+        margin-top: 0
+        margin-bottom: 0
+
+    .address
+        // Address will cut name off when long, so restrict its width
+        max-width: 200px
 
 </style>
