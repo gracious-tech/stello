79e515d2b8c70e23d09dc76af26971d2ed4efb87
shadow-light <noreply@gracious.tech>
2021-03-18 12:04:16 +1100

    Improve sort util

--- app/src/services/utils/arrays.ts
+++ app/src/services/utils/arrays.ts
@@ -1,18 +1,35 @@
 
 
-export function sort(array:any[], property:string=null, ascending:boolean=true):void{
-    // Sort an array by value and optionally by a property (if values are objects)
-    // NOTE Regular array sort converts all values to strings first, so numerical sorting fails
+export function sort(array:string[]|number[]|object[], property:string=null, ascending:boolean=true,
+        ):void{
+    /* Improved sorting of arrays
+        1. Supports both string and number comparison (builtin sort converts all to strings)
+        2. Supports any-case and locale-specific sorting for strings
+        3. Can sort based on a property if items are objects
+    */
+
+    // If empty, can't check first value's type, so return now
+    if (!array.length)
+        return
+
+    // Check type of first value so know what comparison to do
+    const numeric = typeof (property ? array[0][property] : array[0]) === 'number'
+    let compare = numeric ? (a, b) => a - b : new Intl.Collator().compare
+
+    // Reverse comparison if decending order
+    if (!ascending){
+        const orig_compare = compare
+        compare = (a, b) => orig_compare(b, a)
+    }
+
+    // Do the sorting
     array.sort((a, b) => {
         if (property){
+            // Compare based on property if one given
             a = a[property]
             b = b[property]
         }
-        if (a === b){
-            return 0
-        }
-        const comparison = ascending ? a > b : a < b
-        return comparison ? 1 : -1
+        return compare(a, b)
     })
 }
 
