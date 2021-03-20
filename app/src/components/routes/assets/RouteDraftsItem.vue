9b706b14961d6b258d387061c0962d76a5370777
shadow-light <noreply@gracious.tech>
2021-03-20 19:32:50 +1100

    Improve RouteDraftsItem

--- app/src/components/routes/assets/RouteDraftsItem.vue
+++ app/src/components/routes/assets/RouteDraftsItem.vue
@@ -5,15 +5,18 @@ v-list-item(:to='to')
     v-list-item-content
         v-list-item-title
             | {{ draft.display }}
-            v-chip(v-if='is_default' small class='ml-5') default
-    v-list-item-action(v-if='draft.template')
-        app-btn(@click.prevent='copy' icon='post_add')
-    v-list-item-action
+    v-list-item-action(class='flex-row align-center')
+        v-chip(v-if='is_default' small class='mr-3') default
+        v-tooltip(v-if='draft.template' top)
+            | Use for new draft
+            template(#activator='tooltip')
+                app-btn(v-bind='tooltip.attrs' v-on='tooltip.on' @click.prevent='copy' icon='post_add')
         app-menu-more
-            app-list-item(v-if='!draft.template' @click='copy') Copy
+            app-list-item(v-if='!draft.template' @click='copy') Duplicate
             app-list-item(v-if='!draft.template' @click='make_template') Turn into template
-            app-list-item(v-if='draft.template' @click='make_default_template') Make default
-            app-list-item(@click='remove') Remove
+            app-list-item(v-if='draft.template' @click='make_default_template'
+                :disabled='is_default') Make default
+            app-list-item(@click='remove' color='error') Delete
 
 </template>
 
@@ -62,6 +65,10 @@ export default class extends Vue {
 
     remove(){
         self._db.drafts.remove(this.draft.id)
+        // If this was the default template, clear it
+        if (this.draft.id === this.$store.state.default_template){
+            this.$store.commit('dict_set', ['default_template', null])
+        }
         this.$emit('removed', this.draft.id)
     }
 
