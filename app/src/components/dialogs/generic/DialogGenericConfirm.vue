c5e00d44c83fd1a260269ace6007d50a3e13f643
shadow-light <noreply@gracious.tech>
2021-03-26 13:56:52 +1100

    Added DialogGenericConfirm

---
+++ app/src/components/dialogs/generic/DialogGenericConfirm.vue
@@ -0,0 +1,39 @@
+
+<template lang='pug'>
+
+v-card
+    v-card-title {{ title }}
+
+    v-card-actions
+        app-btn(@click='$emit("close")') {{ cancel }}
+        app-btn(@click='$emit("close", true)' :color='confirm_color') {{ confirm }}
+
+</template>
+
+
+<script lang='ts'>
+
+import {Component, Vue, Prop} from 'vue-property-decorator'
+
+
+@Component({})
+export default class extends Vue {
+
+    @Prop({required: true}) title:string
+    @Prop({default: "Confirm"}) confirm:string
+    @Prop({default: "Cancel"}) cancel:string
+    @Prop({default: false}) confirm_danger:boolean
+
+    get confirm_color():string{
+        // The color of the confirm button
+        return this.confirm_danger ? 'error' : 'accent'
+    }
+
+}
+
+</script>
+
+
+<style lang='sass' scoped>
+
+</style>
