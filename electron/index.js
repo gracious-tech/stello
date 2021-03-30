7881ca4cd1bdd0acbdc2fb83a47bb148c5098dff
shadow-light <noreply@gracious.tech>
2021-03-30 15:12:31 +1100

    Handle dns lookup errors

--- electron/index.js
+++ electron/index.js
@@ -264,9 +264,13 @@ function sleep(ms){
 
 ipcMain.handle('dns_mx', async (event, host) => {
     // Do a DNS request for MX records and return domains ordered by priority
-    const results = await dns.resolveMx(host)
-    results.sort((a, b) => a.priority - b.priority)
-    return results.map(record => record.exchange)
+    try {
+        const results = await dns.resolveMx(host)
+        results.sort((a, b) => a.priority - b.priority)
+        return results.map(record => record.exchange)
+    } catch {
+        return []
+    }
 })
 
 
