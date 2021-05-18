3212b2b3ba9dfd23299ed7ec4c523eef357d2f8e
shadow-light <noreply@gracious.tech>
2021-05-18 12:18:53 +1000

    Fix another clipboard bug

--- app/src/services/utils/misc.ts
+++ app/src/services/utils/misc.ts
@@ -15,11 +15,17 @@ declare global {
 export async function get_clipboard_blobs(preferred=[]):Promise<Blob[]>{
     // Return clipboard's contents as blobs
     // NOTE May be multiple clipboard items, so an array is returned
+
+    // Get clipboard items
+    // NOTE In some circumstances a DOMException may occur if no data (possibly only on Windows)
+    const items:ClipboardItem[] = []
+    try {
+        items.push(...await self.navigator.clipboard.read())
+    } catch {}
+
+    // Choose preferred format for each item
     // NOTE Each item may be in multiple formats (e.g. Chrome copy image = [text/html, image/png])
-    // TODO Current browser limitations: prompts for permission, only supports text and pngs?
-    const items = await self.navigator.clipboard.read()
     const blobs = await Promise.all(items.map(item => {
-        // Each clipboard item can have multiple types, so just return the preferred format
         for (const type_prefix of preferred){
             for (const type of item.types){
                 if (type.startsWith(type_prefix)){
@@ -37,9 +43,12 @@ export async function get_clipboard_blobs(preferred=[]):Promise<Blob[]>{
 }
 
 
-export function get_clipboard_text():Promise<string>{
+export function get_clipboard_text():Promise<string|null>{
     // A simpler version of `get_clipboard_blobs` (only returns one item which must be text/plain)
-    return self.navigator.clipboard.readText()
+    try {
+        return self.navigator.clipboard.readText()
+    } catch {}
+    return null
 }
 
 
