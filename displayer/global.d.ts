b0c5aae9053d6dfa80ae02dfa94eca665a62e0aa
shadow-light <noreply@gracious.tech>
2021-05-18 20:48:38 +1000

    Update displayer types

--- displayer/global.d.ts
+++ displayer/global.d.ts
@@ -1,22 +1,29 @@
 
+import {App} from 'vue'
 
-declare module 'CustomTypes' {
 
-    global {
+declare global {
 
-        interface Window {
-            _browser_supported:boolean
-            _hash:string
-            _error_to_msg(error:Error):string
-            _fail(msg:string):void
-            _fail_network():void
-            __fail_alert(heading:string, msg:string):void
-            _app:any
-        }
+    interface String {
+        replaceAll(from:string|RegExp, to:string|Function):string
+    }
 
-        interface String {
-            replaceAll(from:string|RegExp, to:string|Function):string
+    interface ImportMeta {
+        // Extend with vite's env vars that are always exposed
+        env:{
+            MODE:string,
+            BASE_URL:string,
+            PROD:boolean,
+            DEV:boolean,
         }
     }
 
+    interface Window {
+        _browser_supported:boolean
+        _fail_report(msg:string):void
+        _fail_splash(heading:string, msg:string):void
+        _fail_splash_network():void
+        _hash:string
+        _app:App
+    }
 }
--- displayer/src/services/types.ts
+++ displayer/src/services/types.ts
@@ -1,15 +1,2 @@
 
 export type GetAsset = (asset_id:string)=>Promise<ArrayBuffer>
-
-
-declare global {
-    interface ImportMeta {
-        // Extend with vite's env vars that are always exposed
-        env:{
-            MODE:string,
-            BASE_URL:string,
-            PROD:boolean,
-            DEV:boolean,
-        }
-    }
-}
