3a00957bf281f2057ad9b97d33fee2560649124c
shadow-light <noreply@gracious.tech>
2021-07-23 16:06:47 +1000

    Hide manual delete setting

--- app/src/components/routes/RouteProfile.vue
+++ app/src/components/routes/RouteProfile.vue
@@ -47,7 +47,7 @@ div
                 app-integer(v-model='msg_lifespan' v-bind='$t("msg_lifespan")' :min='1' :max='365'
                     infinity)
                 app-integer(v-model='msg_max_reads' v-bind='$t("msg_max_reads")' :min='1' infinity)
-                app-switch(disabled v-model='allow_delete' v-bind='$t("allow_delete")')
+                //- app-switch(disabled v-model='allow_delete' v-bind='$t("allow_delete")')
                 app-switch(v-model='allow_resend_requests' v-bind='$t("allow_resend_requests")')
 
 
