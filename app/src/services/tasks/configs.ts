72a01d7af9c5f97bc8237ce7a374fd39ad4a6b64
shadow-light <noreply@gracious.tech>
2021-07-27 11:40:54 +1000

    Improve efficiency of configs_update task

--- app/src/services/tasks/configs.ts
+++ app/src/services/tasks/configs.ts
@@ -17,34 +17,45 @@ export async function configs_update(task:Task){
     // Setup task
     task.label = `Applying new settings for ${profile.display}`
 
-    // Extract response public key
+    // Done if nothing to do
+    if (!profile.configs_need_uploading){
+        return
+    }
+
+    // Prep for upload tasks
+    const storage = profile.new_host_user()
     const resp_key_public = buffer_to_url64(await export_key(profile.host_state.resp_key.publicKey))
+    let upload_displayer = Promise.resolve()
+    let upload_responder = Promise.resolve()
 
     // Upload displayer config
-    const storage = profile.new_host_user()
-    const disp_config_name = `disp_config_${profile.host_state.disp_config_name}`
-    const upload_displayer = storage.upload_file(disp_config_name, object_to_blob({
-        notify_include_contents: profile.options.notify_include_contents,
-        allow_replies: profile.options.allow_replies,
-        allow_reactions: profile.options.allow_reactions,
-        allow_delete: profile.options.allow_delete,
-        allow_resend_requests: profile.options.allow_resend_requests,
-        social_referral_ban: profile.options.social_referral_ban,
-        resp_key_public,
-        reaction_options: profile.options.reaction_options,
-    }))
+    if (!profile.host_state.displayer_config_uploaded){
+        const disp_config_name = `disp_config_${profile.host_state.disp_config_name}`
+        upload_displayer = storage.upload_file(disp_config_name, object_to_blob({
+            notify_include_contents: profile.options.notify_include_contents,
+            allow_replies: profile.options.allow_replies,
+            allow_reactions: profile.options.allow_reactions,
+            allow_delete: profile.options.allow_delete,
+            allow_resend_requests: profile.options.allow_resend_requests,
+            social_referral_ban: profile.options.social_referral_ban,
+            resp_key_public,
+            reaction_options: profile.options.reaction_options,
+        }))
+    }
 
     // Upload responder config
-    const upload_responder = storage.upload_responder_config({
-        notify_mode: profile.options.notify_mode,
-        notify_include_contents: profile.options.notify_include_contents,
-        allow_replies: profile.options.allow_replies,
-        allow_reactions: profile.options.allow_reactions,
-        allow_delete: profile.options.allow_delete,
-        allow_resend_requests: profile.options.allow_resend_requests,
-        resp_key_public,
-        email: profile.email,
-    })
+    if (!profile.host_state.responder_config_uploaded){
+        upload_responder = storage.upload_responder_config({
+            notify_mode: profile.options.notify_mode,
+            notify_include_contents: profile.options.notify_include_contents,
+            allow_replies: profile.options.allow_replies,
+            allow_reactions: profile.options.allow_reactions,
+            allow_delete: profile.options.allow_delete,
+            allow_resend_requests: profile.options.allow_resend_requests,
+            resp_key_public,
+            email: profile.email,
+        })
+    }
 
     // Wait till both uploads complete
     await task.add(upload_displayer, upload_responder)
