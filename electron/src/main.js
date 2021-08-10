cf7cb1f807a100186e8bc935a5602c880d909107
shadow-light <noreply@gracious.tech>
2021-08-10 14:50:49 +1000

    Fix ip reporting in electron

--- electron/src/main.js
+++ electron/src/main.js
@@ -22,36 +22,37 @@ const CONFIG = JSON.parse(
 
 
 // Report errors to Rollbar in production
-if (app.isPackaged){
-
-    // Define a fake path to app dir (see below)
-    const fake_app_dir = path.sep === '/' ? '/redacted' : 'C:\\redacted'
-
-    Rollbar.init({
-        accessToken: CONFIG.rollbar_electron,
-        captureUncaught: true,
-        captureUnhandledRejections: true,
-        codeVersion: 'v' + app.getVersion(),  // 'v' to match git tags
-        locals: false,  // SECURITY Variable values may expose user data (e.g. username in path)
-        autoInstrument: false,  // SECURITY Don't track use via telemetry to protect privacy
-        captureIp: 'anonymize',  // SECURITY Don't store real IP to protect privacy
-        payload: {
-            platform: 'client',  // Allows using public client token rather than server
-            server: {
-                root: fake_app_dir,  // Required to trigger matching to source code
-                host: 'redacted',  // SECURITY Host may identify user by real name
-            },
+const fake_app_dir = path.sep === '/' ? '/redacted' : 'C:\\redacted'
+Rollbar.init({
+    enabled: app.isPackaged,
+    environment: app.isPackaged ? 'production' : 'development',
+    accessToken: CONFIG.rollbar_electron,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    codeVersion: 'v' + app.getVersion(),  // 'v' to match git tags
+    locals: false,  // SECURITY Variable values may expose user data (e.g. username in path)
+    autoInstrument: false,  // SECURITY Don't track use via telemetry to protect privacy
+    payload: {
+        platform: 'client',  // Allows using public client token rather than server
+        server: {
+            root: fake_app_dir,  // Required to trigger matching to source code
+            host: 'redacted',  // SECURITY Host may identify user by real name
         },
-        onSendCallback: (isUncaught, args, payload) => {
-            // SECURITY Replace all file paths with fake base dir to avoid exposing OS username etc
-            // NOTE `transform` callback is called earlier and doesn't apply to `notifier` prop
-            const app_dir = path.dirname(__dirname)
-            for (const [key, val] of Object.entries(payload)){
-                payload[key] = JSON.parse(JSON.stringify(val).replaceAll(app_dir, fake_app_dir))
-            }
+        request: {
+            // `captureIp` config doesn't work for Node so manually set in payload
+            // See https://github.com/rollbar/rollbar.js/issues/963
+            user_ip: '$remote_ip_anonymize',  // anonymize sets last part of IP to 0 for privacy
         },
-    })
-}
+    },
+    onSendCallback: (isUncaught, args, payload) => {
+        // SECURITY Replace all file paths with fake base dir to avoid exposing OS username etc
+        // NOTE `transform` callback is called earlier and doesn't apply to `notifier` prop
+        const app_dir = path.dirname(__dirname)
+        for (const [key, val] of Object.entries(payload)){
+            payload[key] = JSON.parse(JSON.stringify(val).replaceAll(app_dir, fake_app_dir))
+        }
+    },
+})
 
 
 // Don't open if another instance of Stello is already running
