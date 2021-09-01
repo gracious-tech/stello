68ac5425a01e515433fa07cc4d0960edb095b1a2
shadow-light <noreply@gracious.tech>
2021-09-01 23:38:53 +1000

    Electron tests

---
+++ electron/tests/playwright.config.ts
@@ -0,0 +1,41 @@
+
+import path from 'path'
+
+import {PlaywrightTestConfig, test, _electron as electron, Page} from '@playwright/test'
+
+
+// Extend test interface to start Electron app and use it for tests
+const extended_test = test.extend<{}, {electron_page:Page}>({
+
+    // Worker fixture that provides access to the Electron app's page (set once for whole run)
+    electron_page: [async ({}, use, worker_info) => {
+
+        // Start the electron app
+        const electron_app = await electron.launch({
+            // WARN Testing AppImage requires fuse kernal stuff and therefore docker --privileged
+            // TODO Change path depending on OS
+            executablePath: path.join(__dirname, '../packaged/stello.AppImage'),
+            // TODO Use xvfb-run to run headless
+        })
+
+        // Provide the window/page to tests
+        // NOTE Single window used for all tests to stay close to actual app which forbids multiple
+        await use(await electron_app.firstWindow())
+
+        // Close app when done
+        await electron_app.close()
+
+    }, {scope: 'worker'}],
+
+    // Override standard page fixture with the electron page
+    page: async ({electron_page}, run) => {
+        // Ensure each test begins from root route
+        await electron_page.goto(electron_page.url().split('#')[0] + '#/')
+        await run(electron_page)
+    },
+
+})
+export {extended_test as test}
+
+
+export default {} as PlaywrightTestConfig
