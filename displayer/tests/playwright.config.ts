689e3a5b9d392f8eff16c3e22e2f374779d50481
shadow-light <noreply@gracious.tech>
2021-09-01 21:47:21 +1000

    Run displayer tests in multiple browsers

--- displayer/tests/playwright.config.ts
+++ displayer/tests/playwright.config.ts
@@ -8,4 +8,9 @@ export default {
         port: 8003,
         reuseExistingServer: !process.env['CI'],
     },
+    projects: [
+        {use: {browserName: 'chromium'}},
+        {use: {browserName: 'firefox'}},
+        // TODO Resolve webkit issues https://github.com/microsoft/playwright/issues/8578
+    ],
 } as PlaywrightTestConfig
