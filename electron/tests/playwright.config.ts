44a67eda62e8801a374d5059d8b50c57e23b4e18
shadow-light <noreply@gracious.tech>
2021-09-03 16:41:15 +1000

    Fail on js errors

--- electron/tests/playwright.config.ts
+++ electron/tests/playwright.config.ts
@@ -1,8 +1,39 @@
 
 import path from 'path'
 
-import {PlaywrightTestConfig, test, ElectronApplication, _electron as electron,
+import {PlaywrightTestConfig, test, Page, ElectronApplication, _electron as electron,
     } from '@playwright/test'
+
+
+// Util for logging and failing on js errors
+function attach_js_error_handler(page:Page){
+
+    // Setup a tracker obj that can be updated after returning to caller
+    const js_tracker = {success: true}
+
+    // Util for failing and logging the error
+    const fail = (msg:string) => {
+        console.error(msg)
+        js_tracker.success = false
+    }
+
+    // Log js messages via node, and fail if any are errors
+    page.on('console', msg => {
+        if (msg.type() === 'error'){
+            fail(msg.text())
+        } else {
+            console.log(msg.text())
+        }
+    })
+
+    // Fail if any unhandled exceptions occur
+    page.on('pageerror', error => {
+        fail(`${error.name}: ${error.message}\n${error.stack ?? ''}`)
+    })
+
+    // Return tracker to caller
+    return js_tracker
+}
 
 
 // Config for running tests via a packaged Electron app
@@ -31,12 +62,18 @@ const test_interface_electron = test.extend<unknown, {electron_app:ElectronAppli
     page: async ({electron_app}, run) => {
         // Get the app's window (app will reopen whenever closed when testing, so always present)
         const electron_page = await electron_app.firstWindow()
+        // Listen for JS errors
+        const tracker = attach_js_error_handler(electron_page)
         // Auto click through the welcome splashes
         await electron_page.click('button:has-text("continue")')
         await electron_page.click('.v-input--checkbox')
         await electron_page.click('button:has-text("continue")')
         // Run the test
         await run(electron_page)
+        // Throw if any JS errors occured while testing
+        if (!tracker.success){
+            throw new Error()  // TODO Shows a 404 message rather than actual error
+        }
         // Close page/window when done so that session data is reset (app will auto reopen)
         await electron_page.close()
     },
@@ -60,11 +97,19 @@ const test_interface_port = test.extend({
     // Override standard page fixture to auto click through welcome splashes
     // NOTE Must be same as Electron setup
     page: async ({page}, run) => {
+        // Listen for JS errors
+        const tracker = attach_js_error_handler(page)
+        // Click through welcome splashes
         await page.goto('http://localhost:8000')
         await page.click('button:has-text("continue")')
         await page.click('.v-input--checkbox')
         await page.click('button:has-text("continue")')
+        // Run test
         await run(page)
+        // Throw if any JS errors occured while testing
+        if (!tracker.success){
+            throw new Error()  // TODO Shows a 404 message rather than actual error
+        }
     },
 
 })
