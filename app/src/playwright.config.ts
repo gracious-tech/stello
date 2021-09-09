1535d733e9bae7dbac4da7eb961fe656d438b3e2
shadow-light <noreply@gracious.tech>
2021-09-09 22:33:33 +1000

    unit tests

---
+++ app/src/playwright.config.ts
@@ -0,0 +1,9 @@
+// Unit tests
+// TODO Configure to use tsconfig https://github.com/microsoft/playwright/issues/7121
+
+import {PlaywrightTestConfig} from '@playwright/test'
+
+
+export default {
+
+} as PlaywrightTestConfig
