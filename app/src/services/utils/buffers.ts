6cf8790b3b694f653ec6eb041048b4041e97a67d
shadow-light <noreply@gracious.tech>
2021-09-28 13:04:34 +1000

    join_buffers util

---
+++ app/src/services/utils/buffers.ts
@@ -0,0 +1,14 @@
+
+export function join_buffers(buffers:ArrayBuffer[]):ArrayBuffer{
+    // Join an array of buffers together
+    const total_num_bytes = buffers.reduce((sum, buffer) => sum + buffer.byteLength, 0)
+    const joined_bytes = new Uint8Array(total_num_bytes)
+
+    let offset = 0
+    for (const buffer of buffers){
+        joined_bytes.set(new Uint8Array(buffer), offset)
+        offset += buffer.byteLength
+    }
+
+    return joined_bytes.buffer
+}
