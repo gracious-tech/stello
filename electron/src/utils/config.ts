fb109ead23657cdb3962804a8737f4cafe223d21
shadow-light <noreply@gracious.tech>
2021-11-02 19:44:00 +1100

    HOSTED_API env var

--- electron/src/utils/config.ts
+++ electron/src/utils/config.ts
@@ -14,6 +14,7 @@ export const get_path = (sub_path='') => path.join(__dirname, '../', sub_path)
 // Load config file (created from env vars and embedded during packaging)
 interface ElectronConfig {
     rollbar_electron:string
+    hosted_api:string
 }
 export const CONFIG = JSON.parse(readFileSync(
     get_path('config.json'), {encoding: 'utf8'})) as ElectronConfig
