11e9c3cfe5791666063eb266ab773d8fce4758a6
shadow-light <noreply@gracious.tech>
2021-11-05 11:14:38 +1100

    Rm data URLs

--- displayer/src/services/webp.ts
+++ displayer/src/services/webp.ts
@@ -1,4 +1,6 @@
 
+import {url64_to_buffer} from '@/services/utils/coding'
+
 
 export function check_webp_support():Promise<boolean>{
     // Check whether basic lossy webp is supported
@@ -16,7 +18,9 @@ export function check_webp_support():Promise<boolean>{
             resolve(false)
         }
 
-        // Set source as a minimal sized example
-        img.src = 'data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA'
+        // Set source as a minimal sized example (1:1 pixels)
+        // SECURITY data: URL avoided for security reasons (disabled via CSP)
+        const buffer = url64_to_buffer('UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD-JaQAA3AAAAAA')
+        img.src = URL.createObjectURL(new Blob([buffer], {type: 'image/webp'}))
     })
 }
