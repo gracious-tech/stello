b69b78ab535710982309b61dacbda409318aabef
shadow-light <noreply@gracious.tech>
2021-11-09 10:32:18 +1100

    Update notifications email

--- responder/aws/function/email_template.py
+++ responder/aws/function/email_template.py
@@ -5,49 +5,61 @@ from email_image import base64_image
 
 
 email_template = '''
-<!DOCTYPE html>
 <html>
-    <head></head>
-    <body style='margin: 0; padding: 24px; padding-bottom: 150px; background-color: #222222;'>
-        <div style='border-radius: 12px; max-width: 600px; margin: 0 auto; background-color: #eeeeee; color: #000000;'>
-            <a href='stello://responses'>
-                <img src='data:image/jpeg;base64,{image}' height='1' width='4' style='border-radius: 12px 12px 0 0; width: 100%; height: auto; border-bottom: 1px solid #cccccc;max-height: 150px; background-color: #ddeeff'>
+
+<head>
+    <meta name="color-scheme" content="light dark">
+    <style>
+        @media (prefers-color-scheme: dark) {
+            .button {
+                background-color: #bbddff !important;
+                color: #000000 !important;
+            }
+        }
+    </style>
+</head>
+
+<body style="padding-top:4px;padding-bottom:150px">
+    <div style="border-radius:12px;max-width:600px;margin:0 auto;background-color:rgba(127,127,127,0.15)">
+        <a href="stello://responses">
+            <img src="data:image/jpeg;base64,{image}" height="150" width="600"
+                style="border-radius: 12px 12px 0 0; width: 100%; height: auto; border-bottom: 1px solid #cccccc;max-height: 150px; background-color: #ddeeff">
+        </a>
+        <div style="padding:16px">
+            <p><strong>{heading}</strong></p>
+            <p><br><br>{body}</p>
+        </div>
+
+        <hr style="margin:0;border-style:solid;border-color:#cccccc;border-width:1px 0 0 0">
+
+        <div style="border-radius:0 0 12px 12px;background-color:rgba(87,127,167,0.3);padding:36px 0;text-align:center">
+
+            <a class="button" href="stello://responses"
+                style="background-color:#114488;color:#ffffff;padding:12px 0;border-radius:12px;text-decoration:none;font-family:Roboto,sans-serif">
+                <span style="mso-text-raise: 20pt;">&nbsp;</span>
+                &nbsp;
+                <strong style="mso-text-raise: 10pt;">View in Stello</strong>
+                &nbsp;&nbsp;
             </a>
-            <div style='padding: 24px;'>
-                <p><strong>{heading}</strong></p>
-                <p><br><br>{body}</p>
-            </div>
-
-            <hr style='margin: 0; border-style: solid; border-color: #cccccc; border-width: 1px 0 0 0;'>
-
-            <div style='padding: 12px; border-radius: 0 0 12px 12px; text-align: center;
-                    background-color: #ddeeff; color: #000000; font-family: Roboto, sans-serif;'>
-
-                <p style='margin: 36px 0;'>
-                    <a href='stello://responses' style='background-color: #224477; color: #ffffff;
-                            padding: 12px 18px; border-radius: 12px; text-decoration: none;'>
-                        <strong>View in Stello</strong>
-                    </a>
-                </p>
-
-            </div>
 
         </div>
 
-        <hr style='border-style: none;'>
-        <p>&nbsp;</p>
+    </div>
 
-        <p style='text-align: center; color: #aaaaaa; max-width: 600px; margin: 0 auto;'>
-            <small style="font-size: 0.8em;">
-                Open Stello to identify who responded and to reply to them
-                (not possible via email for security reasons).
-                <br>
-                <br>
-                Open <a href="https://stello.news/" style="color: #aaaaaa;">Stello</a> to customise your notification settings. If you've lost access to your account, or shouldn't be receiving these, <a href="https://gracious.tech/support/stello/" style="color: #aaaaaa;">let us know</a>.
-            </small>
-        </p>
+    <hr style="border-style:none">
+    <p>&nbsp;</p>
+    <p style='text-align: center; color: #aaaaaa; max-width: 600px; margin: 0 auto;'>
+        <small style="font-size: 0.8em;">
+            Open Stello to identify who responded and to reply to them
+            (not possible via email for security reasons).
+            <br>
+            <br>
+            Open <a href="stello://settings" style="color: #aaaaaa;">Stello</a> to customise your notification settings. If you've lost access to your account, or shouldn't be receiving these, <a href="https://gracious.tech/support/stello/" style="color: #aaaaaa;">let us know</a>.
+        </small>
+    </p>
 
-    </body>
+</body>
+
 </html>
 '''
 
