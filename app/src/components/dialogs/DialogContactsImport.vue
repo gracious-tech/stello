e2b078f940b21fa5830fa9f48bc230a0dfdb71cd
shadow-light <noreply@gracious.tech>
2021-12-01 22:58:18 +1100

    Url fix for production

--- app/src/components/dialogs/DialogContactsImport.vue
+++ app/src/components/dialogs/DialogContactsImport.vue
@@ -116,7 +116,8 @@ import {extract_contacts_from_vcard} from '@/services/misc/vcard'
 
 // Configure zip module to use static local worker scripts rather than blobs to avoid CSP issues
 // NOTE Must pass absolute URL otherwise zip.js will try resolve relative to own script and mess up
-const zip_worker_path = new URL(zip_worker, import.meta.url).toString()
+// WARN URL must work in both dev and prod (have very different paths!)
+const zip_worker_path = new URL(zip_worker, self.location.origin).toString()
 zip.configure({
     workerScripts: {
         deflate: [zip_worker_path],
