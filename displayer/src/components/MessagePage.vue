001aa8bf483344a23c4d152161a908f4b8cbde7e
shadow-light <noreply@gracious.tech>
2022-03-20 10:18:59 +1100

    page toolbar ui fix

--- displayer/src/components/MessagePage.vue
+++ displayer/src/components/MessagePage.vue
@@ -5,10 +5,10 @@ transition(appear)
     div.overlay(v-if='page' @click.self='close')
         transition(:name='transition' mode='out-in')
             div.container(:key='page.id')
-                div.toolbar(class='ui')
+                div.toolbar
                     svg(@click='back' viewBox='0 0 24 24')
                         path(d='M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z')
-                    h2 {{ page.content.headline }}
+                    h2(class='ui') {{ page.content.headline }}
                 div.content
                     MessageContents(:sections='page.content.sections')
 
