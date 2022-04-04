d643ef987772c8b98028ae12730abed14b108538
shadow-light <noreply@gracious.tech>
2022-04-04 23:11:57 +1000

    number utils

--- app/src/services/utils/numbers.ts
+++ app/src/services/utils/numbers.ts
@@ -6,3 +6,17 @@ export function enforce_range(value:unknown, smallest:number, largest:number, de
     }
     return Math.min(largest, Math.max(smallest, value))
 }
+
+
+export function parse_int(value:string|number):number|null{
+    // Version of parseInt that returns null if invalid (NaN can't be used in JSON)
+    const result = typeof value === 'number' ? value : Number.parseInt(value, 10)
+    return Number.isNaN(result) ? null : result
+}
+
+
+export function parse_float(value:string|number):number|null{
+    // Version of parseFloat that returns null if invalid (NaN can't be used in JSON)
+    const result = typeof value === 'number' ? value : Number.parseFloat(value)
+    return Number.isNaN(result) ? null : result
+}
