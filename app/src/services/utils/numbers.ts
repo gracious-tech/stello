02697f753c398e30300ae3db7cbecd1038b272c6
shadow-light <noreply@gracious.tech>
2022-04-05 19:11:27 +1000

    Improve number parsers

--- app/src/services/utils/numbers.ts
+++ app/src/services/utils/numbers.ts
@@ -9,16 +9,16 @@ export function enforce_range(value:unknown, smallest:number, largest:number, de
 
 
 export function parse_int(value:unknown):number|null{
-    // Version of parseInt that returns null if invalid (NaN can't be used in JSON)
+    // More flexible version of parseInt that returns null if invalid (NaN can't be used in JSON)
     const number = typeof value === 'number' ? value :
-        (typeof value === 'string' ? Number.parseInt(value, 10) : null)
+        (typeof value === 'string' ? Number.parseInt(value.replace(/[^\d.-]/g, ''), 10) : null)
     return Number.isNaN(number) ? null : number
 }
 
 
 export function parse_float(value:unknown):number|null{
-    // Version of parseFloat that returns null if invalid (NaN can't be used in JSON)
+    // More flexible version of parseFloat that returns null if invalid (NaN can't be used in JSON)
     const number = typeof value === 'number' ? value :
-        (typeof value === 'string' ? Number.parseFloat(value) : null)
+        (typeof value === 'string' ? Number.parseFloat(value.replace(/[^\d.-]/g, '')) : null)
     return Number.isNaN(number) ? null : number
 }
