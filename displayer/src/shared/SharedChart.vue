d62dc69c288a3408ddaa3386e3c6ee728db67083
shadow-light <noreply@gracious.tech>
2022-04-05 22:06:11 +1000

    SharedChart partial

---
+++ displayer/src/shared/SharedChart.vue
@@ -0,0 +1,24 @@
+
+<template lang='pug'>
+
+div.root
+    h2(v-if='title') {{ title }}
+    canvas(ref='canvas')
+    div.cap(v-if='caption') {{ caption }}
+
+</template>
+
+
+<script lang='ts'>
+
+import type {PropType} from 'vue'  // Importing just as type should still keep compatible with Vue 2
+import {defineComponent} from 'vue-demi'
+
+import {parse_float} from '@/services/utils/numbers'
+
+import type {ChartOptions, ChartData, ScriptableContext} from 'chart.js'
+const chartjs_promise = import('chart.js')
+const chartjs_labels_promise = import('chartjs-plugin-datalabels')
+const chartjs_annotation_promise = import('chartjs-plugin-annotation')
+
+
