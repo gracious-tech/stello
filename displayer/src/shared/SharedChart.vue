18349dce7b60be5535fa29de41953b42cf70f22a
shadow-light <noreply@gracious.tech>
2022-04-06 12:45:01 +1000

    Partial SharedChart

--- displayer/src/shared/SharedChart.vue
+++ displayer/src/shared/SharedChart.vue
@@ -16,9 +16,50 @@ import {defineComponent} from 'vue-demi'
 
 import {parse_float} from '@/services/utils/numbers'
 
-import type {ChartOptions, ChartData, ScriptableContext} from 'chart.js'
+import type {Chart, ChartOptions, ChartData, ScriptableContext} from 'chart.js'
 const chartjs_promise = import('chart.js')
 const chartjs_labels_promise = import('chartjs-plugin-datalabels')
 const chartjs_annotation_promise = import('chartjs-plugin-annotation')
 
+
+export default defineComponent({
+
+    props: {
+        type: {
+            type: String as PropType<'bar'|'line'|'doughnut'>,
+            required: true,
+        },
+        data: {
+            type: Array as PropType<{number:string, label:string, hue:number}[]>,
+            required: true,
+        },
+        threshold: {
+            type: String,
+            default: '',
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        caption: {
+            type: String,
+            default: '',
+        },
+        dark: {
+            type: Boolean,
+            required: true,
+        },
+        animate: {
+            type: Boolean,
+            default: true,
+        },
+    },
+
+    chart: null,  // Not in data to prevent Vue from making reactive which messes up Chart internals
+
+    data(){
+        return {
+            register_promise: null as null|Promise<void>,
+        }
+    },
 
