ecdfb6c357827021478e4007a4c7f1f96a76cd6f
shadow-light <noreply@gracious.tech>
2022-04-07 22:21:19 +1000

    AppSwitch hint fix

--- app/src/components/global/AppSwitch.vue
+++ app/src/components/global/AppSwitch.vue
@@ -2,15 +2,16 @@
 
 <template lang='pug'>
 
-div(:class='{both: !!label_false}' class='d-flex align-center')
-    span.label_false(v-if='label_false' @click='toggle' class='v-label mr-2'
-        :class='$store.state.dark ? "theme--dark" : "theme--light"') {{ label_false }}
-    v-switch(
-        v-bind='$attrs'
-        v-model='wrapped_value'
-        :color='$attrs["color"] || "accent"'
-        persistent-hint
-    )
+div(:class='{both: !!label_false}')
+    div(class='d-flex align-center mt-5')
+        span(v-if='label_false' @click='toggle' class='v-label mr-2'
+            :class='$store.state.dark ? "theme--dark" : "theme--light"') {{ label_false }}
+        v-switch(v-bind='$attrs' v-model='wrapped_value' :color='$attrs["color"] || "accent"'
+            persistent-hint)
+        span(@click='toggle' class='v-label ml-2'
+            :class='$store.state.dark ? "theme--dark" : "theme--light"') {{ label }}
+    //- Show own hint so can span whole length across custom labels
+    div.hint(class='v-messages mb-5') {{ hint }}
 
 </template>
 
@@ -24,8 +25,10 @@ import {Component, Vue, Prop} from 'vue-property-decorator'
 export default class extends Vue {
 
     @Prop({type: Boolean, required: true}) declare readonly value:boolean
-    @Prop({type: Boolean, default: null}) declare readonly disabled_value:boolean|null
+    @Prop({type: String, default: null}) declare readonly label:string|null
     @Prop({type: String, default: null}) declare readonly label_false:string|null
+    @Prop({type: Boolean, default: null}) declare readonly disabled_value:boolean|null
+    @Prop({type: String, default: null}) declare readonly hint:string|null
 
     get wrapped_value(){
         // NOTE Can optionally set what value should be when disabled, rather than actual value
@@ -48,7 +51,8 @@ export default class extends Vue {
 
 <style lang='sass' scoped>
 
-.label_false
+.v-label
+    // Clicking labels toggles switch
     user-select: none
     cursor: pointer
 
@@ -60,5 +64,13 @@ export default class extends Vue {
     // Make track of switch neutral to more easily see which side is enabled
     &.v-input--switch__track
         color: hsla(0, 0%, 50%, 0.8) !important
+
+::v-deep
+    // Remove default padding so can self-align
+    .v-input, .v-input__slot, .v-input--selection-controls
+        margin: 0
+        padding: 0
+    .v-messages:not(.hint)
+        display: none  // Don't show switch's inbuilt hint
 
 </style>
