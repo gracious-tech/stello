ab301374e5a03a7c2afad31dbe9919c81c3b5d49
shadow-light <noreply@gracious.tech>
2022-04-07 11:47:30 +1000

    Newline fix

--- displayer/src/shared/SharedChart.vue
+++ displayer/src/shared/SharedChart.vue
@@ -7,8 +7,8 @@ div.root
         canvas(ref='canvas' @click='click')
         div.cap(v-if='caption') {{ caption }}
     svg.placeholder(v-else @click='click' viewBox='0 0 24 24')
-        path(d='M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9
-            17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z')
+        path(d=`M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9
+            17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z`)
 
 </template>
 
