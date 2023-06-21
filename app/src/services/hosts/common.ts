f46f63b51178cecf491d1d42aa7214410b690280
shadow-light <noreply@gracious.tech>
2023-06-21 11:49:56 +1000

    Displayer needs an update

--- app/src/services/hosts/common.ts
+++ app/src/services/hosts/common.ts
@@ -2,7 +2,7 @@
 import {CustomError} from '@/services/utils/exceptions'
 
 
-export const HOST_STORAGE_VERSION = 5  // Bump whenever an update to storage services needed
+export const HOST_STORAGE_VERSION = 6  // Bump whenever an update to storage services needed
 
 
 export class HostPermissionError extends CustomError {}
