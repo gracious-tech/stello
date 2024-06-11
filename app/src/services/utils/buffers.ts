98f5c53be4a72345bc2df0181b5038b1128c35d9
shadow-light <noreply@gracious.tech>
2024-06-11 13:20:49 +1000

    disable_account

--- app/src/services/utils/buffers.ts
+++ app/src/services/utils/buffers.ts
@@ -12,3 +12,23 @@ export function join_buffers(buffers:ArrayBufferLike[]):ArrayBuffer{
 
     return joined_bytes.buffer
 }
+
+
+export function buffers_equal(a:ArrayBufferLike, b:ArrayBufferLike):boolean{
+    // Confirm is two array buffers are the same
+    const aview = new DataView(a)
+    const bview = new DataView(b)
+
+    // First confirm same length, otherwise could throw errors or succeed on partial data
+    if (a.byteLength !== b.byteLength){
+        return false
+    }
+
+    // Compare every byte
+    for (let i = 0; i < a.byteLength; i++){
+        if (aview.getUint8(i) !== bview.getUint8(i)){
+            return false
+        }
+    }
+    return true
+}
