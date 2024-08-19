4d415adf4371868603010c450d62cd5cc4b5475e
shadow-light <noreply@gracious.tech>
2024-08-19 10:28:00 +1000

    electron: store data in documents dir

--- electron/src/setup/data.ts
+++ electron/src/setup/data.ts
@@ -1,6 +1,6 @@
 
 import {join} from 'path'
-import {existsSync} from 'original-fs'
+import {existsSync, renameSync, mkdirSync} from 'original-fs'
 
 import {app} from 'electron'
 
@@ -15,8 +15,32 @@ if (!app.requestSingleInstanceLock()){
 }
 
 
-// Portable support (if data dir exists next to app then use it)
-const portable_data = join(app_path, '..', 'stello_data')
-if (existsSync(portable_data)){
-    app.setPath('userData', portable_data)
+// Determine old data locations (v1.5.3 and below)
+// WARN Do this before setting any paths
+const old_portable_data = join(app_path, '..', 'stello_data')
+const old_user_data = app.getPath('userData')
+
+
+// Possible locations for files
+const files_dir_documents = join(app.getPath('documents'), 'Stello Files')
+const files_dir_portable = join(app_path, '..', 'Stello Files')
+
+
+// Portable support (if files dir exists next to app then use it)
+export const files_dir = existsSync(files_dir_portable) ? files_dir_portable : files_dir_documents
+mkdirSync(files_dir, {recursive: true})  // Ensure exists
+
+
+// Set path for Electron's data
+const electron_user_data = join(files_dir, 'Internal Data')
+app.setPath('userData', electron_user_data)
+
+
+// Move old data if exists and no new data present
+if (!existsSync(electron_user_data)){
+    if (existsSync(old_portable_data)){
+        renameSync(old_portable_data, electron_user_data)
+    } else if (existsSync(old_user_data)){
+        renameSync(old_user_data, electron_user_data)
+    }
 }
