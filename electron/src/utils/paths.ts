dda0bbd611314185329b4aafcefe8e72f0bc034d
shadow-light <noreply@gracious.tech>
2024-10-03 15:09:25 +1000

    electron: correct app_path for portable windows

--- electron/src/utils/paths.ts
+++ electron/src/utils/paths.ts
@@ -4,17 +4,20 @@ import {join} from 'path'
 import {app} from 'electron'
 
 
-// Determine where app is, whether a single file (appimage) or a dir
+// Determine where app is, whether a single file (e.g. appimage) or a dir
 /* getAppPath() results differ by env:
     dev: stello_app/electron
     Linux: tmp location (use APPIMAGE env var)
     Mac: Stello.app/Contents/Resources/app.asar
-    Windows: StelloByGraciousTech/app/resources/app.asar
+    Windows (appx): StelloByGraciousTech/app/resources/app.asar
+    Windows (exe): tmp location
 */
-// NOTE If an AppImage, need to set to the AppImage file rather than currently unpackaged code
+// NOTE If executing from a tmp location, need to set to the packaged file
 let app_path = join(app.getAppPath(), '..')
 if (app.isPackaged){
-    app_path = process.env['APPIMAGE'] || join(app.getAppPath(), '..', '..', '..')
+    app_path = process.env['APPIMAGE']
+        || process.env['PORTABLE_EXECUTABLE_FILE']
+        || join(app.getAppPath(), '..', '..', '..')
 }
 
 export {app_path}
