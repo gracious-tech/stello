05e52603ef8277c230de92f1c57e24318005e174
shadow-light <noreply@gracious.tech>
2024-10-13 19:22:37 +1100

    app: identify profile of subscribe requests

--- app/src/components/reuseable/RequestsSubscribe.vue
+++ app/src/components/reuseable/RequestsSubscribe.vue
@@ -4,8 +4,11 @@
 v-card(v-if='requests.length')
     v-card-title Requests to subscribe
     v-list
-        RequestsSubscribeItem(v-for='request of requests' :key='request.id' :request='request'
-            @removed='removed')
+        template(v-for='[profile_id, profile_display, profiles_requests] of grouped_requests')
+            v-subheader(:key='profile_id' class='justify-center font-weight-bold')
+                | {{ profile_display }}
+            RequestsSubscribeItem(v-for='request of profiles_requests' :key='request.id'
+                :request='request' @removed='removed')
 
 </template>
 
@@ -18,6 +21,7 @@ import RequestsSubscribeItem from './assets/RequestsSubscribeItem.vue'
 import {Task} from '@/services/tasks/tasks'
 import {RecordRequestSubscribe} from '@/services/database/types'
 import {remove_item} from '@/services/utils/arrays'
+import {Profile} from '@/services/database/profiles'
 
 
 @Component({
@@ -25,14 +29,28 @@ import {remove_item} from '@/services/utils/arrays'
 })
 export default class extends Vue {
 
+    profiles:Profile[] = []
     requests:RecordRequestSubscribe[] = []
 
     created(){
         void this.load()
     }
 
+    get grouped_requests(){
+        // Get requests grouped by profile
+        const items:[string, string, RecordRequestSubscribe[]][] = []
+        for (const profile of this.profiles){
+            const profiles_requests = this.requests.filter(r => r.profile === profile.id)
+            if (profiles_requests.length){
+                items.push([profile.id, profile.display, profiles_requests])
+            }
+        }
+        return items
+    }
+
     async load(){
         // Load requests
+        this.profiles = await self.app_db.profiles.list()
         this.requests = await self.app_db._conn.getAll('request_subscribe')
     }
 
