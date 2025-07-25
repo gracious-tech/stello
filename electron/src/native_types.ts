a148261f8c9f2a0bb0d1d60d14d0bf7b695e7063
shadow-light <noreply@gracious.tech>
2025-07-25 21:32:22 +1000

    electron: Switch symlink/target to avoid build bug on MacOS

--- electron/src/native_types.ts
+++ electron/src/native_types.ts
@@ -1,1 +1,51 @@
-../../app/src/services/native/types.ts
+
+export interface NativeInterface {
+    // The methods available for interacting with native platform
+
+    // Functions
+    get_paths():{files_dir:string, data_dir:string}
+    app_file_read(path:string):Promise<ArrayBuffer>
+    user_file_list(path:string):Promise<string[]>
+    user_file_write(path:string, data:ArrayBuffer):Promise<void>
+    user_file_remove(path:string):Promise<void>
+    restart_after_update():void
+    dns_mx(host:string):Promise<string[]>
+    os_encrypt(secret:string):Promise<ArrayBuffer|null>
+    os_decrypt(encrypted:ArrayBuffer):Promise<string|null>
+    html_to_pdf(html:string, filename:string):Promise<null>
+    test_email_settings(settings:EmailSettings, auth?:boolean):Promise<EmailError|null>
+    smtp_send(settings:EmailSettings, email:Email):Promise<EmailError|null>
+
+    // Listeners
+    on_update_ready(handler:()=>void):void
+    on_oauth(handler:(url:string)=>void):void
+}
+
+
+export interface EmailSettings {
+    host:string
+    port:number
+    starttls:boolean
+    user:string
+    pass:string
+}
+
+export interface Email {
+    id:string
+    to:EmailIdentity
+    from:EmailIdentity
+    subject:string
+    html:string
+    reply_to?:EmailIdentity|undefined
+}
+
+export interface EmailIdentity {
+    name:string
+    address:string
+}
+
+export interface EmailError {
+    code:'network'|'dns'|'port'|'starttls_required'|'tls_required'|'auth'|'timeout'|'throttled'
+        |'invalid_to'|'unknown'|'unsupported'
+    details:string
+}
