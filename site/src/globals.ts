c93a8e27bebe7e95dd6a52ff7f1bcf1528f3ac4e
shadow-light <noreply@gracious.tech>
2026-04-14 16:06:34 +1000

    site: update example message url

--- site/src/globals.ts
+++ site/src/globals.ts
@@ -1,2 +1,2 @@
 
-export const EXAMPLE_URL = 'https://xn--ixa2aa3af7a.stello.news/#DU4QDL7V6uvlgCFI9vSLyg~~,U81oCYsRgnjlIjH-Mn4F,N-SwgKsoyUuZHP0veoO87_bSjmsEKZFT_wVvNDXNR2k~'
+export const EXAMPLE_URL = 'https://xn--ixa2aa3af7a.stello.news/#DU4QDL7V6uvlgCFI9vSLyg~~,qMyfKasAurCKWmBPGRCc,FGslpPVfpsadFzS9ySSF9HYpwx_-npZYBdAszH06pHE~'
