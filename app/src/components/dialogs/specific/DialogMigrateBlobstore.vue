1c4c29cf0410b266058c874b3e21bad21238bdcd
shadow-light <noreply@gracious.tech>
2026-05-19 21:46:29 +1000

    app: report migrate fail

--- app/src/components/dialogs/specific/DialogMigrateBlobstore.vue
+++ app/src/components/dialogs/specific/DialogMigrateBlobstore.vue
@@ -6,7 +6,9 @@ v-card(class='pa-6')
     template(v-if='phase === "ready"')
         v-card-title(class='justify-center') Upgrade database
         v-card-text
-            v-alert(v-if='error' text color='error' class='mb-4') {{ error }}
+            v-alert(v-if='error_id' text color='error' class='mb-4')
+                | Upgrade failed. This can happen if Stello can't access the files it needs.
+                | You can retry or #[app-a(:href='support_url') contact us] for help.
             p Stello has updated how images and files are stored. Running this optimization will
                 |  save space on your hard drive and ensure your files can be backed up properly.
             p(class='mt-3 font-weight-bold')
@@ -37,6 +39,7 @@ v-card(class='pa-6')
 import {Component, Vue} from 'vue-property-decorator'
 
 import {migrate} from '@/services/database/blobstore_migrate'
+import {error_to_string} from '@/services/utils/exceptions'
 
 
 @Component({})
@@ -45,14 +48,19 @@ export default class extends Vue {
     phase:'ready'|'running'|'complete' = 'ready'
     progress = 0
     total = 0
-    error = ''
+    error_id = ''
 
     get percent(){
         return this.total ? (this.progress / this.total * 100) : 0
     }
 
+    get support_url(){
+        const desc = `Stello blobstore_migrate failed.\nError id: ${this.error_id}`
+        return `https://gracious.tech/contact?desc=${encodeURIComponent(desc)}`
+    }
+
     async start(){
-        this.error = ''
+        this.error_id = ''
         this.phase = 'running'
         try {
             await migrate((progress, total) => {
@@ -62,8 +70,9 @@ export default class extends Vue {
             this.$store.commit('dict_set', ['show_blobstore_migrate', false])
             this.phase = 'complete'
         } catch (error){
-            console.error(error)
-            this.error = "Upgrade failed. You can try again if desired."
+            const error_str = error_to_string(error)
+            this.error_id =
+                self.app_report_error(`blobstore_migrate failed\n${error_str}`) || 'unknown'
             this.phase = 'ready'
         }
     }
